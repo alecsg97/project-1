a0d6737c8fd4b3f592a3f6ec1c6911d7a2ed7e17
Alec Santamaria <alec@Alecs-MBP.attlocal.net>
2022-08-09 21:44:38 -0400

    first commit

---
+++ src/script.js
@@ -0,0 +1,71 @@
+let errors = 0;
+
+const listOfEasyPuzzles = {
+    easy1: [[9, ' ', ' ', 2, ' ', ' ', 4, ' ', ' '],
+            [' ', 3, 2, ' ', 4, ' ', ' ', ' ', ' '],
+            [' ', 4, ' ', 8, ' ', ' ', ' ', ' ', 7],
+            [' ', ' ', 7, ' ', 5, ' ', ' ', ' ', 9],
+            [' ', ' ', 1, 7, ' ', 2, 3, ' ', ' '],
+            [3, ' ', ' ', ' ', 8, ' ', 1, 7, ' '],
+            [5, ' ', ' ', ' ', ' ', 9, ' ', 8, ' '],
+            [' ', ' ', ' ', ' ', 2, 8, 5, 4, ' '],
+            [' ', ' ', 8, ' ', ' ', ' ', ' ', ' ', 1]
+],
+    easy2: [[]
+],
+    easy3: [[]
+]
+}
+
+const listOfMediumPuzzles = {
+    medium1: [[]
+],
+    medium2: [[]
+],
+    medium3: [[]
+]
+}
+
+const listOfHardPuzzles = {
+    hard1: [[]
+],
+    hard2: [[]
+],
+    hard3: [[]
+]
+}
+
+const solutionListOfEasyPuzzles = {
+    solutionEasy1: [[9, 7, 6, 2, 1, 5, 4, 3, 8],
+                    [8, 3, 2, 9, 4, 7, 6, 1, 5],
+                    [1, 4, 5, 8, 6, 3, 9, 2, 7],
+                    [4, 2, 7, 3, 5, 1, 8, 6, 9],
+                    [6, 8, 1, 7, 9, 2, 3, 5, 4],
+                    [3, 5, 9, 4, 8, 6, 1, 7, 2],
+                    [5, 1, 4, 6, 7, 9, 2, 8, 3],
+                    [7, 9, 3, 1, 2, 8, 5, 4, 6],
+                    [2, 6, 8, 5, 3, 4, 7, 9, 1]
+],
+    solutionEasy2: [[]
+],
+    solutionEasy3: [[]
+]
+}
+
+const solutionListOfMediumPuzzles = {
+    solutionMedium1: [[]
+],
+    solutionMedium2: [[]
+],
+    solutionMedium3: [[]
+]
+}
+
+const solutionListOfHardPuzzles = {
+    solutionHard1: [[]
+],
+    solutionHard2: [[]
+],
+    solutionHard3: [[]
+]
+}
